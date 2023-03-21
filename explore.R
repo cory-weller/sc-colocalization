c4b61d8163dde081a27c3fa2e0c153e73653ca6a
Cory Weller <cory.a.weller@gmail.com>
2023-03-21 14:37:36 -0400

    run coloc per chr

--- explore.R
+++ explore.R
@@ -8,6 +8,7 @@ registerDoMC(cores=4)
 library(ggplot2)
 library(ggthemes)
 library(viridis)
+library(coloc)
 
 # read in formatted summary statistics - gr38
 
@@ -33,36 +34,117 @@ celltypes <- list(c('basal_ganglia','data/Basalganglia-EUR-out.cis.summary.txt')
                 c('spinal_cord','data/Spinalcord-out.cis.summary.txt')
 )
 
+# coloc can specify sdY=1 if expression was normalized to standard normal distribution
+
 compiled_table_fn <- 'merged_hits.tsv'
+
+DT[, varbeta = se^2 ]
+
+dat[, varbeta := se^2]
+
+diagnoses <- c( 'AD'='data/AD_Bellenguez.formatted.tsv.gz',
+                'LBD'='data/LBD_Chia.formatted.tsv.gz',
+                'PD'='data/PD_Nalls.formatted.tsv.gz')
+
+celltypes <- c( 'basal_ganglia'='data/Basalganglia-EUR-out.cis.summary.txt',
+                'cerebellum'='data/Cerebellum-out.cis.summary.txt',
+                'cortex'='data/Cortex-EUR-out.cis.summary.txt',
+                'hippocampus'='data/Hippocampus-out.cis.summary.txt',
+                'spinal_cord'='data/Spinalcord-out.cis.summary.txt')
+
 
 
 get_diagnosis <- function(diagnosis_fn, diagnosis_name) {
     dat <- fread(diagnosis_fn)
     dat[, c('SNP','A1','A2') := NULL]
-    setnames(dat, c('BETA','FRQ','SE','P'), c('diagnosis_b','diagnosis_Freq', 'diagnosis_se', 'diagnosis_p'))
-    setcolorder(dat, c('CHR','BP','diagnosis_Freq','diagnosis_b','diagnosis_se','diagnosis_p'))
+    setnames(dat, c('BETA','FRQ','SE','P'), c('beta','Freq', 'se', 'p'))
     dat[, 'diagnosis' := diagnosis_name]
+    setkey(dat, CHR, BP)
     return(dat[])
 }
 
 get_eQTL <- function(celltype_fn, celltype_name) {
     dat <- fread(celltype_fn)
     dat <- dat[SNP_Chr != 'SNP_Chr']
+    dat[,c('TopSNP','A1','A2','cis_Chr','cis_left','cis_right','Orientation','Probe_Chr','Probe', 'Probe_bp', 'Gene') := NULL]
     setnames(dat, 'SNP_Chr', 'CHR')
+    dat <- dat[order(CHR,BP,p)]
+    dat <- dat[which(!duplicated(dat[,c(1:2)]))]        # get non-duplicate
     dat[, CHR := as.numeric(CHR)]
     dat[, BP := as.numeric(BP)]
     dat[, p := as.numeric(p)]
     dat[, se := as.numeric(se)]
     dat[, b := as.numeric(b)]
     dat[, Freq := as.numeric(Freq)]
-    dat[,c('TopSNP','A1','A2','cis_Chr','cis_left','cis_right','Orientation','Probe_Chr','Probe', 'Probe_bp', 'Gene') := NULL]
-    setnames(dat, c('Freq','b','se','p'), c('eQTL_Freq','eQTL_b','eQTL_se','eQTL_p'))
+    setnames(dat, c('Freq','b','se','p'), c('Freq','beta','se','p'))
     setkey(dat, CHR, BP)
     dat[, 'celltype' := celltype_name]
     return(dat[])
 }
 
 
+run_coloc <- function(diagnosis.DT, eQTL.DT, chromosome) {
+    d <- copy(diagnosis.DT)[CHR==chromosome]    # subset by chromosome
+    diagnosis <- list()
+    diagnosis$position <- d$BP
+    diagnosis$snp <- paste0(chromosome, '_', d$BP)
+    diagnosis$beta <- d$beta
+    diagnosis$varbeta <- d$se^2
+    diagnosis$type <- 'cc'
+
+
+    e <- copy(eQTL.DT)[CHR==chromosome]
+    eQTL <- list()
+    eQTL$position <- e$BP
+    eQTL$snp <- paste0(chromosome, '_', e$BP)
+    eQTL$beta <- e$beta
+    eQTL$varbeta <- e$se^2
+    eQTL$type <- 'quant'
+    eQTL$sdY <- rep(1, length(eQTL$snp))
+
+    res <- coloc.abf(diagnosis, eQTL)
+    res <- res$results
+    setDT(res)
+    return(res)
+}
+
+o <- foreach(d_fn=diagnoses, d_name=names(diagnoses), .combine='rbind') %do% {
+    diagnosis.DT <- get_diagnosis(d_fn, d_name)
+    foreach(e_fn=celltypes, e_name=names(celltypes), .combine='rbind') %do% {
+        eQTL.DT <- get_eQTL(e_fn, e_name)
+        o1 <- foreach(chromosome=1:22, .combine='rbind') %do% {
+            run_coloc(diagnosis.DT, eQTL.DT, chromosome)
+        }
+        o1[, 'celltype' := e_name]
+        o1[, 'diagnosis' := d_name]
+        return(o1[])
+    }
+}
+
+saveRDS(o, file='coloc.RDS')
+
+o.signif <- o[SNP.PP.H4 > 0.5, .SD, .SDcols=c('chr','position','diagnosis','celltype','SNP.PP.H4')]
+o.signif <- o.signif[order(diagnosis, celltype, chr, position, SNP.PP.H4)]
+fwrite(o.signif, file='coloc_50_PP.tsv', quote=F, row.names=F, col.names=T, sep='\t')
+
+
+o[, chr := tstrsplit(snp, split='_')[1]]
+o[, chr := as.numeric(chr)]
+
+
+o2 <- order(o$SNP.PP.H4,decreasing=TRUE)
+cs <- cumsum(o$SNP.PP.H4[o2])
+w <- which(cs > 0.95)[1]
+my.res$results[o,][1:w,]$snp
+
+ggplot(o[SNP.PP.H4>0.95], aes(x=position, y=-1*log10(SNP.PP.H4), color=diagnosis, shape=celltype)) + geom_point() + facet_grid(chr~.) + theme_few()
+
+subset(o,SNP.PP.H4>0.95)
+
+check_dataset(eQTL)
+check_dataset(diagnosis)
+
+res <- coloc.abf(diagnosis, eQTL)
 
 if (! file.exists(compiled_table_fn)) {
     dat <- foreach(i=diagnoses, .combine='rbind') %do% {
@@ -77,25 +159,24 @@ if (! file.exists(compiled_table_fn)) {
             return(dt.merge[])
         }
     }
+    dat <- dat[eQTL_p < 0.05 & diagnosis_p < 0.05]
+    #dat[, eQTL_hit := ifelse(eQTL_p < 0.05, TRUE, FALSE)]
+    #dat[, diagnosis_hit := ifelse(diagnosis_p < 0.05, TRUE, FALSE)]
 
-    dat[, eQTL_hit := ifelse(eQTL_p < 0.05, TRUE, FALSE)]
-    dat[, diagnosis_hit := ifelse(diagnosis_p < 0.05, TRUE, FALSE)]
+    # chrLengths <- data.table(CHR=1:22, bp=
+    # c('248956422','242193529','198295559','190214555','181538259','170805979','159345973',
+    # '145138636','138394717','133797422','135086622','133275309','114364328','107043718',
+    # '101991189','90338345','83257441','80373285','58617616','64444167','46709983','50818468'))
 
-    chrLengths <- data.table(CHR=1:22, bp=
-    c('248956422','242193529','198295559','190214555','181538259','170805979','159345973',
-    '145138636','138394717','133797422','135086622','133275309','114364328','107043718',
-    '101991189','90338345','83257441','80373285','58617616','64444167','46709983','50818468'))
+    # chrLengths[, cumulative := shift(cumsum(bp), type='lag', fill=0)]
+    # chrLengths[, bp := NULL]
 
-    chrLengths[, cumulative := shift(cumsum(bp), type='lag', fill=0)]
-    chrLengths[, bp := NULL]
-
-    dat  <- merge(dat, chrLengths, by.x='CHR', by.y='CHR')
+    # dat  <- merge(dat, chrLengths, by.x='CHR', by.y='CHR')
     fwrite(dat, file='merged_hits.tsv', quote=F, row.names=F, col.names=T, sep='\t')
 } else {
     dat <- fread(compiled_table_fn)
 }
 
-dat <- dat[diagnosis_hit==TRUE & eQTL_hit==TRUE][order(celltype, diagnosis)]
 
 dat[, grp := paste0(celltype, '_', diagnosis)]
 
@@ -138,6 +219,7 @@ for(chromosome in 1:22) {
 
 }
 
+setkey(dat, CHR, BP)
 
 # format GTF
 
@@ -151,6 +233,7 @@ split_keys <- function(x, all_keys) {
         key_ <- gsub("'", '', key_)
         value_ <- gsub('"', '', split_pair[2])
         value_ <- gsub("'", '', value_)
+        value_ <- unlist(value_)
         a <- data.frame('tmp'=value_)
         setnames(a, 'tmp', key_)
         out_cols <- c(out_cols, a)
@@ -185,6 +268,28 @@ b <- foreach(i=gtf$V9, .combine='rbind') %do% {
 
 gtf <- cbind(gtf, b)
 gtf <- gtf[gene_biotype=='protein_coding']
+
+if(length(unique(
+            nrow(gtf),
+            length(unlist(gtf$gene_name)),
+            length(unlist(gtf$gene_id)),
+            length(unlist(gtf$gene_version)),
+            length(unlist(gtf$gene_source)),
+            length(unlist(gtf$gene_biotype))
+            )
+) != 1) {
+    cat('Row lengths for newly formed GTF are not equal!\n')
+    quit(1)
+}
+
+
+
+
+gtf[, gene_name := unlist(gene_name)]
+gtf[, gene_id := unlist(gene_id)]
+gtf[, gene_version := unlist(gene_version)]
+gtf[, gene_source := unlist(gene_source)]
+gtf[, gene_biotype := unlist(gene_biotype)]
 gtf[, V9 := NULL]
 
 
@@ -203,12 +308,12 @@ dat[, idx := 1:.N]
 setkey(gtf, CHR, start, stop)
 overlaps <- foverlaps(dat, gtf, type="within")[!is.na(V3)]
 
-exonic_hits <- dat[! idx %in% unique(overlaps$idx)]
-exonic_hits[, c('start','stop') := NULL]
+noncoding_hits <- dat[! idx %in% unique(overlaps$idx)]
+#noncoding_hits[, c('start','stop') := NULL]
 
 # SO FAR SO GOOD
 
-setkey(exonic_hits, CHR, start)
+setkey(noncoding_hits, CHR, start)
 setkey(gtf, CHR, start)
 
 
@@ -224,8 +329,8 @@ minus_search <- function(x, y) {
 }
 
 
-foreach(chr=1:22, .combine='rbind') %do% {
-    dt.chr <- exonic_hits[CHR==chr]
+o <- foreach(chr=1:22, .combine='rbind') %do% {
+    dt.chr <- noncoding_hits[CHR==chr]
     dt.gtf.plus <- gtf[CHR==chr & V7 == '+']
     dt.gtf.minus <- gtf[CHR==chr & V7 == '-']
     plus_idx <- sapply(dt.chr$BP, function(x) plus_search(x, dt.gtf.plus$start))
@@ -242,9 +347,22 @@ foreach(chr=1:22, .combine='rbind') %do% {
 
     dt.chr[, dist_to_plus_start := plus_start - BP]
     dt.chr[, dist_to_minus_start := BP - minus_start ]
-    dt.chr[, 'closest_gene_id' := ifelse(dist_to_plus_start - dist_to_minus_start, plus_gene_id, minus_gene_id)]
-    dt.chr[, 'closest_gene_name' := ifelse(dist_to_plus_start - dist_to_minus_start, plus_gene_name, minus_gene_name)]
+    dt.chr[, 'closest_gene_id' := ifelse(dist_to_plus_start < dist_to_minus_start, plus_gene_id, minus_gene_id)]
+    dt.chr[, 'closest_gene_name' := ifelse(dist_to_plus_start < dist_to_minus_start, plus_gene_name, minus_gene_name)]
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 want next MINUS position just below that
