12b35ebc0438e78efa7750132b5e01d82d51d3b2
Cory Weller <cory.a.weller@gmail.com>
2023-03-30 09:25:35 -0400

    remove extra headers

--- src/submitSMR.sh
+++ src/submitSMR.sh
@@ -1,38 +1,35 @@
 #!/usr/bin/env bash
 
 TISSUE=${1}
+PVAL=${2}
+
 SMR='/data/CARD/projects/singlecell_humanbrain/coloc/smr-1.3.1'
-OUTDIR="/data/CARD/projects/singlecell_humanbrain/coloc_20230322/"
+OUTDIR="/gpfs/gsfs9/users/wellerca/sc-colocalization/data/TISSUE_eQTL"
 TMPDIR="/lscratch/${SLURM_JOB_ID}"
 eQTL_DIR='/data/CARD/projects/omicSynth/SMR_omics/eQTLs'
 FILESTEM="2020-05-26-${TISSUE}"
 BESD="${eQTL_DIR}/${FILESTEM}/${FILESTEM}-${CHR}-SMR-besd"
 
-# bash ~/sc-colocalization/submitSMR.sh Basalganglia-EUR
-# bash ~/sc-colocalization/submitSMR.sh Cerebellum-EUR
-# bash ~/sc-colocalization/submitSMR.sh Cortex-AFR
-# bash ~/sc-colocalization/submitSMR.sh Cortex-EUR
-# bash ~/sc-colocalization/submitSMR.sh Hippocampus-EUR
-# bash ~/sc-colocalization/submitSMR.sh Spinalcord-EUR
-
-
-run_smr() {
+retrieve_smr() {
     local chr=${1}
+    local pval=${2}
     local besd="${eQTL_DIR}/${FILESTEM}/${FILESTEM}-${chr}-SMR-besd"
-    echo "Running SMR for ${TISSUE} chr ${chr}"
+    echo "retrieving SMR for ${TISSUE} chr ${chr}"
 
     # print ALL eQTL results regardless of p-value
     ${SMR} \
         --descriptive-cis \
         --beqtl-summary \
         ${besd} \
-        --query 1 \
+        --query ${pval} \
         --out ${TISSUE}_${chr}
     
-    echo "gzipping output to ${OUTDIR}/${TISSUE}_${chr}.tsv.gz"
-    # cleanup
-    gzip -c ${TISSUE}_${chr}.txt > ${OUTDIR}/${TISSUE}_${chr}.tsv.gz
-    rm ${TISSUE}_${chr}.txt
+    if [ ! -f "${OUTDIR}/${TISSUE}.tsv" ]; then
+        cat ${TISSUE}_${chr}.txt > ${OUTDIR}/${TISSUE}.tsv
+    else
+        awk 'NR > 1' ${TISSUE}_${chr}.txt >> ${OUTDIR}/${TISSUE}.tsv
+    fi
+        rm ${TISSUE}_${chr}.txt
 }
 
 
@@ -43,7 +40,10 @@ mkdir -p ${TMPDIR} && cd ${TMPDIR}
 echo "Tissue type ${TISSUE}"
 
 for CHR in $(seq 1 22); do
-    run_smr ${CHR}
+    retrieve_smr ${CHR} ${PVAL}
 done
+
+echo 'gzipping...'
+gzip ${OUTDIR}/${TISSUE}.tsv
 
 echo "all done"
